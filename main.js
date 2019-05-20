1c3bc6bc648fec5f3998b272f31c384098d7d411
tanabee <tanabee1986@gmail.com>
2019-05-20 17:24:54 +0900

    全てのチケットを取得できるようにした

--- main.js
+++ main.js
@@ -2,20 +2,33 @@ var API_TOKEN    = PropertiesService.getScriptProperties().getProperty("API_TOKE
 var SUB_DOMAIN   = PropertiesService.getScriptProperties().getProperty("SUB_DOMAIN");
 var MAIL_ADDRESS = PropertiesService.getScriptProperties().getProperty("MAIL_ADDRESS");
 
-function main() {
-  var response = apiRequestToZendesk('tickets.json', '?sort_by=created_at&sort_order=desc');
-  var tickets = response.tickets.map(function (ticket) {
-    return [
-      ticket.id,
-      ticket.brand_id,
-      ticket.subject,
-      ticket.description,
-      ticket.tags.join(','),
-      ticket.created_at,
-      ticket.updated_at,
-    ];
-  });
-  Logger.log(tickets);
+function fetchTickets() {
+  /*
+  var tickets = SpreadsheetApp.getActiveSpreadsheet()
+    .getSheetByName("tickets")
+    .getDataRange()
+    .getValues();
+  var latestTicketId = tickets[1][0];// データの先頭行の 1 列目
+  */
+
+  var tickets = [];
+  var page = 1;
+  do {
+    var response = apiRequestToZendesk('tickets.json', '?sort_by=created_at&sort_order=desc&page=' + page);
+    tickets = tickets.concat(response.tickets.map(function (ticket) {
+      return [
+        ticket.id,
+        ticket.brand_id,
+        ticket.subject,
+        ticket.description,
+        ticket.tags.join(','),
+        ticket.created_at,
+        ticket.updated_at,
+      ];
+    }));
+    page++;
+  } while (response.next_page != null);
+  tickets.unshift(['ID', 'Brand ID', 'Subject', 'Description', 'Tags', 'created_at', 'updated_at']);
   SpreadsheetApp.getActiveSpreadsheet()
     .getSheetByName("tickets")
     .getRange('A1:G' + tickets.length)
